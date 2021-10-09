8dab9a3d56670b15b887909cee4dbc4d2214b96a
frozen90 <pych60@gmail.com>
2021-10-09 19:53:00 +0100

    Welcome screen prepared for the user when there is no notes.

--- src/components/Notes/Notes.js
+++ src/components/Notes/Notes.js
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react";
 import { API, graphqlOperation } from 'aws-amplify';
 import { listNotes } from '../../graphql/queries';
-import { Container, Grid, Menu, Segment, Icon, Form , Button} from 'semantic-ui-react'
+import { Container, Grid, Segment, Icon, Loader, Button, Header } from 'semantic-ui-react'
 import './ExtraCssNotes.css'
 import { Helmet } from "react-helmet";
+
+
 export const Notes = () => {
 
+    const [loading, setLoading] = useState(true)
     const [notes, setNotes] = useState([])
     useEffect(() => {
         fetchNotes()
@@ -16,26 +19,41 @@ export const Notes = () => {
             const notesData = await API.graphql(graphqlOperation(listNotes))
             const notes = notesData.data.listNotes.items
             setNotes(notes)
+            setLoading(false)
         } catch (err) {
             console.log('error')
         }
     }
 
     return (
-        <Container fluid>
-            <Grid centered style={{ padding: '50px' }} verticalAlign="middle">
-                <Grid.Column computer={16} mobile={16} tablet={16} className="notes-dashboard">
-                    <Helmet>
-                        <meta charSet="utf-8" />
-                        <title>Notes Dashboard</title>
-                        <link rel="canonical" href="/login" />
-                    </Helmet>
-                    <Segment textAlign='center' style={{ background:'transparent'}}>
 
-                       
-                    </Segment>
-                </Grid.Column>
-            </Grid>
+        <Container fluid>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Notes Dashboard</title>
+                <link rel="canonical" href="/login" />
+            </Helmet>
+            {!loading ?
+                <Grid centered style={{ padding: '50px', height: "100vh" }} verticalAlign="middle">
+                    {notes.length > 0 ?
+                        null
+                        :
+                        <Grid.Column computer={16} mobile={16} tablet={16} className="notes-dashboard">
+                            <Segment textAlign='center' style={{ marginTop: '100px', background: 'transparent' }}>
+                                <Icon size="massive" name='book' />
+                                <Header as="h1" inverted>Nordcloud notes</Header>
+                                <p style={{ color: 'white' }}>Take notes, reminders, share them with friends in secure manner</p>
+                                <Button className="get-started-btn" content='Get Started' />
+                            </Segment>
+                        </Grid.Column>
+                    }
+                </Grid>
+
+                :
+                <Loader active />
+            }
+
+
         </Container>
     )
 }
