1c721eb829560c87b850ccf05d90af392d53a843
frozen90 <pych60@gmail.com>
2021-10-10 13:22:03 +0100

    Saving progress.@

--- src/components/Notes/components/Note.js
+++ src/components/Notes/components/Note.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { Header, Card, Icon, Button } from "semantic-ui-react";
+import { Header, Card, Icon, Button, TextArea } from "semantic-ui-react";
 export const Note = () => {
 
-
+    const text = "Smutny paragraf o zyciu i przemijaniu...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ex consequat, interdum sem ac, ornare enim. Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac"
     return (
         <Card className='note-bg'>
             <Card.Content textAlign='center'><Header as="h2" className='header-card'>Title</Header></Card.Content>
             <Card.Content textAlign='left' className='note-content'>
-                <p>
-                    Smutny paragraf o zyciu i przemijaniu...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ex consequat, interdum sem ac, ornare enim. Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac
-                </p>
+                <input value={text} rows={25} className='remove-bg note-textarea' style={{width: 350}}>
+                    
+                </input>
             </Card.Content>
             <Card.Content extra>
 
