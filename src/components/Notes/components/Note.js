06d327d827d6a43d5544a754980ee88ad5fb7a06
frozen90 <pych60@gmail.com>
2021-10-10 14:12:27 +0100

    remove scroll bar from textarea.

--- src/components/Notes/components/Note.js
+++ src/components/Notes/components/Note.js
@@ -3,7 +3,7 @@ import { Header, Card, Icon, Button } from "semantic-ui-react";
 import TextareaAutosize from 'react-textarea-autosize';
 export const Note = () => {
     const [editable, setEditable] = useState(false)
-    const [value, setValue] = useState("Smutny paragraf o zyciu i przemijaniu...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ex consequat, interdum sem ac, ornare enim. Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac")
+    const [value, setValue] = useState("Smutny paragraf o zyciu i przemijaniu...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ex consequat, interdum sem ac, ornare enim. Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac")
     const textAreaRef = React.createRef();
     const handleTextChange = (e) =>{
         setValue(e.target.value)
@@ -19,7 +19,7 @@ export const Note = () => {
         <Card className='note-bg'>
             <Card.Content textAlign='center' className='card-header-content'><Header as="h2" className='header-card'>Title</Header></Card.Content>
             <Card.Content textAlign='left' className='note-content'>
-                <TextareaAutosize ref={textAreaRef} onDoubleClick={()=>{setEditable(true)}} value={value} onChange={handleTextChange} maxRows={8} className='remove-bg note-textarea' readOnly={!editable}/>
+                <TextareaAutosize ref={textAreaRef} onDoubleClick={()=>{setEditable(true)}} value={value} onChange={handleTextChange} maxRows={10} className='remove-bg note-textarea' readOnly={!editable}/>
             </Card.Content>
             <Card.Content extra>
 
