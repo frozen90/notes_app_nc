2899c83020b553b269e87db327d5ccb6760a9a87
frozen90 <pych60@gmail.com>
2021-10-10 14:01:04 +0100

    Edit button to work on click and focus textarea

--- src/components/Notes/components/Note.js
+++ src/components/Notes/components/Note.js
@@ -1,19 +1,29 @@
-import React from "react";
-import { Header, Card, Icon, Button, TextArea } from "semantic-ui-react";
+import React, { useState } from "react";
+import { Header, Card, Icon, Button } from "semantic-ui-react";
+import TextareaAutosize from 'react-textarea-autosize';
 export const Note = () => {
-
-    const text = "Smutny paragraf o zyciu i przemijaniu...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ex consequat, interdum sem ac, ornare enim. Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac"
+    const [editable, setEditable] = useState(false)
+    const [value, setValue] = useState("Smutny paragraf o zyciu i przemijaniu...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ex consequat, interdum sem ac, ornare enim. Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac")
+    const textAreaRef = React.createRef();
+    const handleTextChange = (e) =>{
+        setValue(e.target.value)
+       
+    }
+    const focusTextArea = () => {
+        setEditable(true)
+        textAreaRef.current.selectionStart = textAreaRef.current.value.length;
+        textAreaRef.current.selectionEnd = textAreaRef.current.value.length;
+        textAreaRef.current.focus();
+    }
     return (
         <Card className='note-bg'>
-            <Card.Content textAlign='center'><Header as="h2" className='header-card'>Title</Header></Card.Content>
+            <Card.Content textAlign='center' className='card-header-content'><Header as="h2" className='header-card'>Title</Header></Card.Content>
             <Card.Content textAlign='left' className='note-content'>
-                <input value={text} rows={25} className='remove-bg note-textarea' style={{width: 350}}>
-                    
-                </input>
+                <TextareaAutosize ref={textAreaRef} onDoubleClick={()=>{setEditable(true)}} value={value} onChange={handleTextChange} maxRows={8} className='remove-bg note-textarea' readOnly={!editable}/>
             </Card.Content>
             <Card.Content extra>
 
-                <Button className='remove-bg' floated='right'><Button.Content><Icon size='large' inverted name='edit'></Icon></Button.Content></Button>
+                <Button className='remove-bg' floated='right' onClick={focusTextArea}><Button.Content><Icon size='large' inverted name='edit'></Icon></Button.Content></Button>
             </Card.Content>
         </Card>
     )
