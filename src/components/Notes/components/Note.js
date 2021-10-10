24599bd6478ec23fe87e4631259eec0f97cf5d94
frozen90 <pych60@gmail.com>
2021-10-10 14:17:11 +0100

    More styling, lock function btn added.

--- src/components/Notes/components/Note.js
+++ src/components/Notes/components/Note.js
@@ -5,9 +5,9 @@ export const Note = () => {
     const [editable, setEditable] = useState(false)
     const [value, setValue] = useState("Smutny paragraf o zyciu i przemijaniu...Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed ex consequat, interdum sem ac, ornare enim. Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac Pellentesque sit amet tortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac ortor turpis. Cras congue tristique ligula eleifend rutrum. Nam sit amet dui in leo hendrerit euismod eu eget dui. Vestibulum ac")
     const textAreaRef = React.createRef();
-    const handleTextChange = (e) =>{
+    const handleTextChange = (e) => {
         setValue(e.target.value)
-       
+
     }
     const focusTextArea = () => {
         setEditable(true)
@@ -19,11 +19,12 @@ export const Note = () => {
         <Card className='note-bg'>
             <Card.Content textAlign='center' className='card-header-content'><Header as="h2" className='header-card'>Title</Header></Card.Content>
             <Card.Content textAlign='left' className='note-content'>
-                <TextareaAutosize ref={textAreaRef} onDoubleClick={()=>{setEditable(true)}} value={value} onChange={handleTextChange} maxRows={10} className='remove-bg note-textarea' readOnly={!editable}/>
+                <TextareaAutosize ref={textAreaRef} onDoubleClick={() => { setEditable(true) }} value={value} onChange={handleTextChange} maxRows={10} className='remove-bg note-textarea' readOnly={!editable} />
             </Card.Content>
             <Card.Content extra>
-
+                <Button className='remove-bg' floated='right' onClick={focusTextArea}><Button.Content><Icon size='large' inverted name='lock'></Icon></Button.Content></Button>
                 <Button className='remove-bg' floated='right' onClick={focusTextArea}><Button.Content><Icon size='large' inverted name='edit'></Icon></Button.Content></Button>
+
             </Card.Content>
         </Card>
     )
